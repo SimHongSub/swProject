a97eeabe710e5f6ca9923c47c1aff76530b7b935
SimHongsub <ghdtjq2038@gmail.com>
2020-06-12 20:44:36 +0900

    add main class

---
+++ src/com/marketplace/main/Main.java
@@ -0,0 +1,14 @@
+package com.marketplace.main;
+
+import com.marketplace.login.controller.LoginController;
+import com.marketplace.view.Login;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//LoginController login = new LoginController();
+		
+		LoginController.showView();
+	}
+
+}
