64e22793e6550cfb465ff010fec909f0a8ef97b1
SimHongsub <ghdtjq2038@gmail.com>
2020-06-12 20:43:49 +0900

    add models(User, Book)

---
+++ src/com/marketplace/model/Book.java
@@ -0,0 +1,55 @@
+package com.marketplace.model;
+
+public class Book {
+	private String isbn;
+	private String title;
+	private String author;
+	private String publisher;
+	private String date;
+	private int price;
+	
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	private String state;
+
+}
