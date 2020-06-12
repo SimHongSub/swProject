eb4615c344983173921ee7a06740bacab6c16a32
SimHongsub <ghdtjq2038@gmail.com>
2020-06-12 20:43:28 +0900

    add login controller class

---
+++ src/com/marketplace/login/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.marketplace.login.controller;
+
+import com.marketplace.view.Login;
+
+public class LoginController {
+	
+	private static Login view = new Login();
+	
+	public LoginController() {
+		
+	}
+	
+	public static void showView() {
+		view.show();
+	}
+	
+	public static void loginClick() {
+		
+	}
+	
+	public static void signUpClick() {
+		view.close();
+		SignUpController.showView();
+	}
+
+}
