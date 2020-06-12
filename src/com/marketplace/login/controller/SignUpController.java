8889747cf5a3d2b2fbebc86a394c793c57eec167
SimHongsub <ghdtjq2038@gmail.com>
2020-06-12 20:44:22 +0900

    add sign up controller class

---
+++ src/com/marketplace/login/controller/SignUpController.java
@@ -0,0 +1,12 @@
+package com.marketplace.login.controller;
+
+import com.marketplace.view.SignUp;
+
+public class SignUpController {
+	private static SignUp view = new SignUp();
+	
+	public static void showView() {
+		view.show();
+	}
+
+}
