20cf0711fcb36184a2c8f87d51efe0278bbe196d
SimHongsub <ghdtjq2038@gmail.com>
2020-06-14 01:03:34 +0900

    add source to show book list view

--- src/com/marketplace/controller/MainController.java
+++ src/com/marketplace/controller/MainController.java
@@ -1,16 +1,26 @@
 package com.marketplace.controller;
 
+import java.util.ArrayList;
+
+import com.marketplace.filesystem.FileProcessing;
+import com.marketplace.model.Book;
 import com.marketplace.view.Main;
 
 public class MainController {
 
 	private Main view;
+	private ArrayList<Book> bookList;
+	FileProcessing fp;
 	
 	public MainController() {
 		view = new Main();
+		bookList = new ArrayList<Book>();
+		fp = new FileProcessing();
+		
+		fp.readBookFile("bookInfo", bookList);
 	}
 	
 	public void showview() {
-		view.show();
+		view.show(bookList);
 	}
 }
