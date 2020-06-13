32ab36f72d15d0489b17a8960749e325b08dc268
SimHongsub <ghdtjq2038@gmail.com>
2020-06-14 02:40:36 +0900

    add source to main page view and function

--- src/com/marketplace/controller/MainController.java
+++ src/com/marketplace/controller/MainController.java
@@ -1,26 +1,55 @@
 package com.marketplace.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.marketplace.filesystem.FileProcessing;
+import com.marketplace.main.MarketPlace;
 import com.marketplace.model.Book;
+import com.marketplace.model.User;
 import com.marketplace.view.Main;
 
 public class MainController {
 
 	private Main view;
 	private ArrayList<Book> bookList;
+	private ArrayList<User> userList;
 	FileProcessing fp;
 	
 	public MainController() {
 		view = new Main();
 		bookList = new ArrayList<Book>();
+		userList = new ArrayList<User>();
 		fp = new FileProcessing();
 		
 		fp.readBookFile("bookInfo", bookList);
+		fp.readUserFile("userInfo", userList);
 	}
 	
 	public void showview() {
 		view.show(bookList);
+	}
+	
+	public HashMap<String, String> purchase(int index){
+		HashMap<String, String> result = new HashMap<String, String>();
+		
+		Book book = bookList.get(index);
+		
+		String sellerId = book.getUserId();
+		String sellerEmail = null;
+		for(int i=0; i<userList.size(); i++) {
+			User user = userList.get(i);
+			
+			if(user.getId().equals(sellerId)) {
+				sellerEmail = user.getEmail();
+				
+				break;
+			}
+		}
+		
+		result.put("message", "구매자 " + MarketPlace.my.getId() + " E-mail : " + MarketPlace.my.getEmail() + "\n"
+				+ "판매자 " + sellerId + " E-mail : " + sellerEmail + "\n메일이 발송되었습니다.");
+		
+		return result;
 	}
 }
--- src/com/marketplace/view/Main.java
+++ src/com/marketplace/view/Main.java
@@ -1,20 +1,32 @@
 package com.marketplace.view;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.HashMap;
 
+import javax.swing.AbstractCellEditor;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellEditor;
+import javax.swing.table.TableCellRenderer;
 
+import com.marketplace.controller.AdminController;
+import com.marketplace.controller.MainController;
 import com.marketplace.filesystem.FileProcessing;
+import com.marketplace.main.MarketPlace;
 import com.marketplace.model.Book;
 
 public class Main {
@@ -25,42 +37,102 @@ public class Main {
 	}
 
 	public void show(ArrayList<Book> list) {
-		JPanel pn = new JPanel();
-		/*BorderLayout bordelLayout = new BorderLayout();
+		// table 초기화
+		String header[] = {"ISBN", "제목", "저자", "출판사", "출판년도", "상태", "가격", "판매자", ""};
+		DefaultTableModel model = new DefaultTableModel(header, 0);
 		
-		pn.setLayout(bordelLayout);*/
+		for(int i=0; i<list.size(); i++) {
+			Book book = list.get(i);
+			String info[] = {book.getIsbn(), book.getTitle(), book.getAuthor(), book.getPublisher(), book.getDate(), book.getState(), book.getPrice(), book.getUserId()};
+			
+			model.addRow(info);
+		}
+		
+		JTable table = new JTable(model);
+		
+		table.getColumnModel().getColumn(8).setCellRenderer(new TableCell(table));
+		table.getColumnModel().getColumn(8).setCellEditor(new TableCell(table));
+		
+		JScrollPane scrollPane = new JScrollPane(table);
 		
 		//frame 사이즈, 화면상 위치 설정
 		frm.setSize(1000, 700);
 		frm.setLocationRelativeTo(null);
 				
 		//frame layout설정
-		//frm.setLayout(new GridLayout(3, 2));
+		frm.setLayout(new BorderLayout());
 		
-		String header[] = {"ISBN", "제목", "저자", "출판사", "출판년도", "상태", "가격", "판매자"};
-		String[][] contents = new String[list.size()][8];
+
+		JPanel northPanel = new JPanel();
+		//northPanel.setLayout(mgr);
 		
-		for(int i=0; i<list.size(); i++) {
-			Book book = list.get(i);
+		JButton searchBtn = new JButton("검색");
+		
+		northPanel.add(searchBtn);
+		
+		if(MarketPlace.my.getAuthority().equals("ADMIN")) {
+			JButton adminBtn = new JButton("관리자 페이지");
+			adminBtn.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					AdminController adminController = new AdminController();
+					
+					frm.dispose();
+					adminController.showView();
+				}
+			});
 			
-			contents[i][0] = book.getIsbn();
-			contents[i][1] = book.getTitle();
-			contents[i][2] = book.getAuthor();
-			contents[i][3] = book.getPublisher();
-			contents[i][4] = book.getDate();
-			contents[i][5] = book.getState();
-			contents[i][6] = book.getPrice();
-			contents[i][7] = book.getUserId();
+			northPanel.add(adminBtn);
+		}else {
+			JButton myBtn = new JButton("My Book");
+			northPanel.add(myBtn);
 		}
 		
-		JTable table = new JTable(contents, header);
-		JScrollPane scrollPane = new JScrollPane(table);
-		
+		frm.add(northPanel, BorderLayout.NORTH);
 		frm.add(scrollPane);
-				
-		//component 생성
-		JButton signUpBtn = new JButton("회원가입");
 		
 		frm.setVisible(true);
 	}
+	
+	@SuppressWarnings("serial")
+	class TableCell extends AbstractCellEditor implements TableCellEditor, TableCellRenderer {
+        JButton purchaseBtn;
+ 
+        public TableCell(JTable table) {
+        	purchaseBtn = new JButton("구입");
+        	
+        	purchaseBtn.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					HashMap<String, String> result = new HashMap<String, String>();
+					MainController mainController = new MainController();
+					
+					result = mainController.purchase(table.getSelectedRow());
+					
+					JOptionPane.showMessageDialog(frm, result.get("message"), "Message", JOptionPane.INFORMATION_MESSAGE, null);
+				}
+			});
+        }
+ 
+        @Override
+        public Object getCellEditorValue() {
+            return null;
+        }
+ 
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+                int row, int column) {
+
+            return purchaseBtn;
+        }
+ 
+        @Override
+        public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row,
+                int column) {
+
+            return purchaseBtn;
+        }
+    }
 }
