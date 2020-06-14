b9e2b7e3821ff17c63d25a37a28f4be60162e156
SimHongsub <ghdtjq2038@gmail.com>
2020-06-15 00:09:50 +0900

    add class to add button in main page table

---
+++ src/com/marketplace/view/table/MainTableCell.java
@@ -0,0 +1,92 @@
+package com.marketplace.view.table;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+
+import javax.swing.AbstractCellEditor;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.TableCellEditor;
+import javax.swing.table.TableCellRenderer;
+
+import com.marketplace.controller.MainController;
+import com.marketplace.main.MarketPlace;
+
+@SuppressWarnings("serial")
+public class MainTableCell extends AbstractCellEditor implements TableCellEditor, TableCellRenderer  {
+	JButton purchaseBtn;
+    JButton deleteBtn;
+    
+    public MainTableCell(JTable table, JFrame frm) {
+    	if(MarketPlace.my.getAuthority().equals("ADMIN")) {
+    		deleteBtn = new JButton("삭제");
+    		deleteBtn.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					MainController mainController = new MainController();
+					
+					int check =JOptionPane.showConfirmDialog(frm, "정말 삭제하시겠습니까?", "확인", JOptionPane.YES_NO_OPTION);
+					
+					if(check == JOptionPane.YES_OPTION) {
+						mainController.delete(table.getSelectedRow());
+						
+						frm.dispose();
+						mainController.showView();
+    				}
+					
+					
+				}
+			});
+    	}else {
+    		purchaseBtn = new JButton("구입");
+        	
+        	purchaseBtn.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					HashMap<String, String> result = new HashMap<String, String>();
+					MainController mainController = new MainController();
+					
+					result = mainController.purchase(table.getSelectedRow());
+					
+					JOptionPane.showMessageDialog(frm, result.get("message"), "Message", JOptionPane.INFORMATION_MESSAGE, null);
+				}
+			});	
+    	}
+    	
+    }
+
+    @Override
+    public Object getCellEditorValue() {
+        return null;
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+            int row, int column) {
+    	
+    	if(MarketPlace.my.getAuthority().equals("ADMIN")) {
+    		return deleteBtn;
+    	}else {
+            return purchaseBtn;	
+    	}
+    }
+
+    @Override
+    public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row,
+            int column) {
+    	
+    	if(MarketPlace.my.getAuthority().equals("ADMIN")) {
+    		return deleteBtn;
+    	}else {
+
+            return purchaseBtn;        		
+    	}
+
+    }
+}
