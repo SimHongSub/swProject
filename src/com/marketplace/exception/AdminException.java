36b38e0b623e2fcacbcfc90cf81a09ddf081b451
SimHongsub <ghdtjq2038@gmail.com>
2020-06-19 12:19:02 +0900

    add comment AdminException

--- src/com/marketplace/exception/AdminException.java
+++ src/com/marketplace/exception/AdminException.java
@@ -1,5 +1,12 @@
 package com.marketplace.exception;
 
+/** 
+ * Class responsible for admin controller exception.
+ * 
+ * @date 2020.06.10
+ * @author SimHongSub
+ * @version 1.0
+ */
 @SuppressWarnings("serial")
 public class AdminException extends Exception {
 	/**
