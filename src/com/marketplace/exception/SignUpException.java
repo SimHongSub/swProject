738482b35ab4e0d8d3acf31850b58be477edd8da
SimHongsub <ghdtjq2038@gmail.com>
2020-06-19 12:19:49 +0900

    add comment SignUpException

--- src/com/marketplace/exception/SignUpException.java
+++ src/com/marketplace/exception/SignUpException.java
@@ -1,5 +1,12 @@
 package com.marketplace.exception;
 
+/** 
+ * Class responsible for signup controller exception.
+ * 
+ * @date 2020.06.10
+ * @author SimHongSub
+ * @version 1.0
+ */
 @SuppressWarnings("serial")
 public class SignUpException extends Exception {
 	/**
