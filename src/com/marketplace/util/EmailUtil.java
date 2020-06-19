ac20fb157f2f775925a006b5698e0a7c1c47adee
SimHongsub <ghdtjq2038@gmail.com>
2020-06-19 12:21:52 +0900

    add comment EmailUtil

--- src/com/marketplace/util/EmailUtil.java
+++ src/com/marketplace/util/EmailUtil.java
@@ -2,11 +2,24 @@ package com.marketplace.util;
 
 import com.marketplace.main.MarketPlace;
 
+/** 
+ * Class that supports E-mail service.
+ * 
+ * @date 2020.06.12
+ * @author SimHongSub
+ * @version 1.0
+ */
 public class EmailUtil {
 
-	public String send(String user, String email) {
+	/**
+	 * Method to send email.
+	 * 
+	 * @param userId - User id to receive email.
+	 * @param email - Email address to receive email.
+	 */
+	public String send(String userId, String email) {
 		String message = "구매자 " + MarketPlace.my.getId() + " E-mail : " + MarketPlace.my.getEmail() + "\n"
-				+ "판매자 " + user + " E-mail : " + email + "\n메일이 발송되었습니다.";
+				+ "판매자 " + userId + " E-mail : " + email + "\n메일이 발송되었습니다.";
 		
 		return message;
 	}
