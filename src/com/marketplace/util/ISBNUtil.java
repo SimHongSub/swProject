77287dc4d4a3711535191d706379f9964459d16c
SimHongsub <ghdtjq2038@gmail.com>
2020-06-19 12:22:00 +0900

    add comment ISBNUtil

--- src/com/marketplace/util/ISBNUtil.java
+++ src/com/marketplace/util/ISBNUtil.java
@@ -4,8 +4,19 @@ import java.text.SimpleDateFormat;
 
 import com.marketplace.main.MarketPlace;
 
+/** 
+ * Class that supports ISBN service.
+ * 
+ * @date 2020.06.12
+ * @author SimHongSub
+ * @version 1.0
+ */
 public class ISBNUtil {
 
+	/**
+	 * Method to create ISBN number.
+	 * 
+	 */
 	public String create() {
 		String nowTime = new SimpleDateFormat("YYYYMMDDHHmmss").format(System.currentTimeMillis());
 		String isbn = MarketPlace.my.getId() + nowTime;
