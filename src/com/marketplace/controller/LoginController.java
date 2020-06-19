3b35f80324d095f01902a969e989a1268b29c627
SimHongsub <ghdtjq2038@gmail.com>
2020-06-19 12:18:25 +0900

    add comment LoginController

--- src/com/marketplace/controller/LoginController.java
+++ src/com/marketplace/controller/LoginController.java
@@ -5,13 +5,26 @@ import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.marketplace.exception.LoginException;
 import com.marketplace.filesystem.FileProcessing;
 import com.marketplace.main.MarketPlace;
 import com.marketplace.model.User;
 import com.marketplace.util.AES256Util;
 import com.marketplace.view.Login;
 
+/** 
+ * Class responsible for login page backend logic.
+ * 
+ * @date 2020.06.12
+ * @author SimHongSub
+ * @version 1.0
+ */
 public class LoginController {
+	/**
+	 * view - General view object.
+	 * userList - List to saved user object.
+	 * fp - FileProcessing object to read file.
+	 */
 	private Login view;
 	private ArrayList<User> userList;
 	private FileProcessing fp;
@@ -24,56 +37,74 @@ public class LoginController {
 		fp.readUserFile("userInfo", userList);
 	}
 	
-	public void showView() {
-		view.show();
+	/**
+	 * Method showing the view.
+	 * 
+	 * @exception LoginException
+	 */
+	public void showView() throws LoginException{
+		try {
+			view.show();
+		}catch (Exception e) {
+			new LoginException("Login controller showView() method exception.");
+		}	
 	}
 	
-	public HashMap<String, String> loginCheck(String id, String pw) {
+	/**
+	 * Method to check user login information.
+	 * 
+	 * @param id - input user id information.
+	 * @param pw - input user password information.
+	 * @return HashMap<String, String>
+	 * @exception LoginException
+	 */
+	public HashMap<String, String> loginCheck(String id, String pw) throws LoginException{
 		HashMap<String, String> result = new HashMap<String, String>();
 		
-		for(int i=0; i<userList.size(); i++) {
-			User user = userList.get(i);
-			String decryptedPassword = null;
-			
-			if(id.equals(user.getId())){
-				try {
-					AES256Util aesUtil = new AES256Util();
-					
+		try {
+			for(int i=0; i<userList.size(); i++) {
+				User user = userList.get(i);
+				String decryptedPassword = null;
+				
+				if(id.equals(user.getId())){
 					try {
-						decryptedPassword = aesUtil.decrypt(user.getPassword());
-					} catch (GeneralSecurityException e) {
-						// TODO Auto-generated catch block
+						AES256Util aesUtil = new AES256Util();
+						
+						try {
+							decryptedPassword = aesUtil.decrypt(user.getPassword());
+						} catch (GeneralSecurityException e) {
+							e.printStackTrace();
+						}
+					} catch (UnsupportedEncodingException e) {
 						e.printStackTrace();
 					}
-				} catch (UnsupportedEncodingException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				if(decryptedPassword.equals(pw)) {
-					if(user.getState().equals("activated")) {
-						result.put("message", "success");
-						
-						MarketPlace.my = user;
-						
-						return result;
+					
+					if(decryptedPassword.equals(pw)) {
+						if(user.getState().equals("activated")) {
+							result.put("message", "success");
+							MarketPlace.my = user;
+							
+							return result;
+						}else {
+							result.put("message", "관리자로부터 로그인이 제한되었습니다.");
+							
+							return result;
+						}
 					}else {
-						result.put("message", "관리자로부터 로그인이 제한되었습니다.");
+						result.put("message", "비밀번호를 잘못 입력하셨습니다.");
 						
 						return result;
 					}
-					
-				}else {
-					result.put("message", "비밀번호를 잘못 입력하셨습니다.");
-					
-					return result;
 				}
 			}
+			
+			result.put("message", "없는 아이디입니다.");
+			
+			return result;
+		}catch (Exception e) {
+			new LoginException("Login controller loginCheck() method exception.");
+			
+			return null;
 		}
-		
-		result.put("message", "없는 아이디입니다.");
-		
-		return result;
 	}
-
 }
