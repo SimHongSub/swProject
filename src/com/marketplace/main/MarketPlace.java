2eeea6729c085ddc71cc37bc1a8c8efc658e1ba4
SimHongsub <ghdtjq2038@gmail.com>
2020-06-19 12:20:14 +0900

    add comment MarketPlace

--- src/com/marketplace/main/MarketPlace.java
+++ src/com/marketplace/main/MarketPlace.java
@@ -1,18 +1,30 @@
 package com.marketplace.main;
 
 import com.marketplace.controller.LoginController;
+import com.marketplace.exception.LoginException;
 import com.marketplace.model.User;
 
+/** 
+ * Class responsible for marketplace system start.
+ * 
+ * @date 2020.06.12
+ * @author SimHongSub
+ * @version 1.0
+ */
 public class MarketPlace {
-	
+	/**
+	 * my - login user object
+	 */
 	public static User my;
 
 	public static void main(String[] args) {
 		LoginController loginController = new LoginController();
 		
-		loginController.showView();
-		/*char[] test = {'1', '2', '3'};
-		
-		loginController.loginCheck("admin", test);*/
+		try {
+			loginController.showView();
+		} catch (LoginException e) {
+			e.printStackTrace();
+		}
 	}
 }
+
